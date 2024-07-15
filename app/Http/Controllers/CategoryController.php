01c57ddb54844b92f08a00d07f3095d71ce10ef3
NishesStha22 <sthanisi2020@gmail.com>
2024-07-15 13:16:15 +0545

    changes on controller and api of category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -37,6 +37,50 @@ class CategoryController extends Controller
             throw $th;
         }
     }
+    public function show($id){
+        try {
+            $showCategory = Category::where('id', $id)->where('status',true)->first();
+            return response ()-> json([
+                'status' => 1,
+                'data' => $showCategory
+            ]);
+            
+        } catch (\Throwable $th) {
+            throw $th;
+        } 
+    }
+    public function update(Request $request, $id){
+        try {
+            $UpdateCat = Category::where('id', $id)->where('status', true)->first();
+            $UpdateCat->categoriesID = $request->categoriesID;
+            $UpdateCat->categoriesName = $request->categoriesName;
+            $UpdateCat->save();
 
+            return response ()-> json ([
+                'status' => 1,
+                'message' => 'Categories  Are Updated Successfully',
+                'data' => $UpdateCat
+
+            ]);
+
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+    }
+
+    public function destory($id){
+        try {
+            $destoryCat = Category::where('id', $id)->where('status', 1)->first();
+            $destoryCat->status = false;
+            $destoryCat->save();
+            return response()->json([
+                'status' => 1,
+                'message' => 'Delete A Categories',
+                'data' => $destoryCat
+            ]);
+        } catch (\Throwable $th) {
+            throw $th;
+        }  
+    }
 
 }
